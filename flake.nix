80d419c31995192cc90499478855d421c704f199
Kevin Michael <kevin.vncnt@gmail.com>
2024-07-05 09:59:04 +1200

    Creating a portable Dev environment, with R packages, dev-tools and other utilities

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+
+  description = "Global packages for R, Zig, and Neovim";
+
+
+
+  inputs = {
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";# Current stable relese
+
+  };
+
+
+
+  outputs = { self, nixpkgs }: let
+
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs { inherit system; };
+
+  in {
+
+    packages.${system}.default = pkgs.buildEnv {
+
+      name = "global-packages";
+
+      paths = with pkgs; [
+
+        R
+        quarto
+
+        rPackages.tidyverse
+        rPackages.data_table
+        rPackages.arrow
+        rPackages.languageserver
+        rPackages.odbc
+        rPackages.shiny
+
+        pipx
+        python312Packages.radian
+
+        julia
+        go
+        rustup
+        conda
+
+        zig
+        cmake
+        libgcc
+
+        neovim
+        yazi
+        fzf
+        ripgrep
+        fd
+        fastfetch
+
+      ];
+
+    };
+
+  };
+
+}
